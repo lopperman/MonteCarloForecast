0f06f89e7a35585916ea54d541d68f6ec955c745
Brower <s0043995@rgare.net>
2016-08-02 07:58:34 -0500

    added initial solution - pb

---
+++ MonteCarloForecastWin/MonteCarloForecastTest/TrialTest.cs
@@ -0,0 +1,31 @@
+﻿using MonteCarloForecast;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarloForecastTest
+{
+    [TestFixture]
+    public class TrialTest
+    {
+        Trial trial = null;
+
+        [OneTimeSetUp]
+        public void setup()
+        {
+            trial = new Trial();
+        }
+
+        [OneTimeTearDown]
+        public void teardown()
+        {
+            trial = null;
+        }
+
+
+
+    }
+}
