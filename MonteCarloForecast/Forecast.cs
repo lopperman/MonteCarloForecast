5a374a86489c12b08fc66f6e0abbb4a180fc6da6
Brower <s0043995@rgare.net>
2016-08-02 17:40:45 -0500

    cleaned up namespace usage - pb

--- MonteCarloForecast/Forecast.cs
+++ MonteCarloForecast/Forecast.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MonteCarloForecast
 {
     public class Forecast
     {
+        Random rnd = new Random();
+
         public DateTime StartDt { get; set; }
         public int RemainingStoriesGuessLow { get; set; }
         public int RemainingStoriesGuessHigh { get; set; }
@@ -21,6 +20,64 @@ namespace MonteCarloForecast
             RemainingStoriesGuessHigh = remainingStoriesHighGuess;
             SplitProbabilityLow = splitProbabilityLow;
             SplitProbabilityHigh = splitProbabilityHigh;
+        }
+
+        public Random Rnd
+        {
+            get
+            {
+                return rnd;
+            }
+        }
+
+        public List<ForecastResult> GetForecastBasedOnHistory(int modelSize, int[] samples)
+        {
+
+            double[] weeksToZero = new double[modelSize];
+
+            for (int i = 0; i < modelSize; i ++)
+            {
+                Trial trial = new Trial(Rnd);
+                trial.LowStoryRemainingEstimate = RemainingStoriesGuessLow;
+                trial.HighStoryRemainingEstimate = RemainingStoriesGuessHigh;
+                trial.LowSplitProbability = SplitProbabilityLow;
+                trial.HighSplitProbability = SplitProbabilityHigh;
+                trial.StartDate = StartDt;
+                TrialResult trialResult = trial.RunTrialBasedOnHistoricSamples(samples);
+                weeksToZero[i] = trialResult.Results.Count - 1;
+
+                if (i > modelSize) break;
+            }
+
+            return BuildForecastResults(weeksToZero);
+        }
+
+        public List<ForecastResult> BuildForecastResults(double[] array)
+        {
+            List<ForecastResult> ret = new List<ForecastResult>();
+
+            List<double> previousValues = new List<double>();
+            previousValues.Add(0);
+
+            int i = 0;
+
+            int weeks = 1;
+            while (true)
+            {
+                double results = MathUtilities.PercentileRank(array, weeks);
+
+                ret.Add(new ForecastResult(Convert.ToDecimal(results), weeks, StartDt.AddDays(7 * weeks)));
+
+                weeks += 1;
+
+                if (results >= 100)
+                {
+                    break;
+                }
+
+            }
+
+            return ret;
         }
     }
 }
--- MonteCarloForecastWin/MonteCarloForecastTest/ForecastTest.cs
+++ MonteCarloForecastWin/MonteCarloForecastTest/ForecastTest.cs
@@ -2,9 +2,6 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MonteCarloForecastTest
 {
@@ -30,7 +27,26 @@ namespace MonteCarloForecastTest
 
         }
 
+        [Test]
+        public void GetForecastBasedOnHistory()
+        {
+            DateTime StartDt = DateTime.Now;
+            int RemainingStoriesGuessLow = 40;
+            int RemainingStoriesGuessHigh = 45;
+            double SplitProbabilityLow = 1.0;
+            double SplitProbabilityHigh = 1.0;
+            int[] samples = new int[] { 5, 6, 3, 1, 18, 13, 5, 4 };
+            int modelSize = 10000;
 
+            Forecast f = new Forecast(StartDt, RemainingStoriesGuessLow, RemainingStoriesGuessHigh, SplitProbabilityLow, SplitProbabilityHigh);
+
+            Assert.IsNotNull(f.Rnd);
+
+            List<ForecastResult> results = f.GetForecastBasedOnHistory(modelSize, samples);
+
+
+
+        }
 
 
 
